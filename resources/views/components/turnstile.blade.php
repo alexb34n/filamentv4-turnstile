2d0f80f788c1335c1590800c72acd2a3c51ab24a
Alex Bean <mralexbean@gmail.com>
2025-08-29 11:37:40 +0100

    v4 changes+1

--- resources/views/components/turnstile.blade.php
+++ resources/views/components/turnstile.blade.php
@@ -1,5 +1,5 @@
 <x-dynamic-component
-    :component="$getFieldWrapperComponent()"
+    :component="$getFieldWrapperView()"
     :field="$field"
 >
     <div x-data="{ state: $wire.entangle('{{ $getStatePath() }}') }"
